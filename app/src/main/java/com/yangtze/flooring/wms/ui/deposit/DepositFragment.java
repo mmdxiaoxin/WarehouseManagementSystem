f5e5e442e964097ccbaae4c7d5777ae6fd432619
小新 <782446723@qq.com>
2024-01-26 15:16:47 +0800

    feat:测试使用RecyclerView来实现记录，优化加载速度

--- app/src/main/java/com/yangtze/flooring/wms/ui/deposit/DepositFragment.java
+++ app/src/main/java/com/yangtze/flooring/wms/ui/deposit/DepositFragment.java
@@ -1,62 +1,114 @@
 package com.yangtze.flooring.wms.ui.deposit;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+import androidx.swiperefreshlayout.widget.SwipeRefreshLayout;
 
 import com.yangtze.flooring.wms.R;
-import com.yangtze.flooring.wms.components.LockTable.NoscrollListView;
-import com.yangtze.flooring.wms.components.LockTable.SyncHorizontalScrollView;
-import com.yangtze.flooring.wms.model.Record;
-import com.yangtze.flooring.wms.adapter.RecordDataAdapter;
-import com.yangtze.flooring.wms.adapter.RecordDataLeftAdapter;
+import com.yangtze.flooring.wms.components.LockTablePlus.ContentAdapter;
+import com.yangtze.flooring.wms.components.LockTablePlus.Entity;
+import com.yangtze.flooring.wms.components.LockTablePlus.TopTabAdpater;
 
+import java.util.ArrayList;
 import java.util.List;
 
 
 public class DepositFragment extends Fragment {
 
     private DepositViewModel depositViewModel;
-    private NoscrollListView mLeft;
-    private RecordDataLeftAdapter mRecordDataLeftAdapter;
-
-    private NoscrollListView mData;
-    private RecordDataAdapter mRecordDataAdapter;
-
-    private SyncHorizontalScrollView mHeaderHorizontal;
-    private SyncHorizontalScrollView mDataHorizontal;
-
-    private List<Record> mListData;
+    private TextView tvLeftTitle;
+    private RecyclerView rvTabRight;
+    private LinearLayout llTopRoot;
+    private RecyclerView recyclerContent;
+    private SwipeRefreshLayout swipeRefresh;
+    private List<Entity> mEntities = new ArrayList<>();
+    private List<String> rightMoveDatas = new ArrayList<>();
+    private List<String> topTabs = new ArrayList<>();
 
 
+    @SuppressLint("MissingInflatedId")
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
         depositViewModel = new ViewModelProvider(this).get(DepositViewModel.class);
         View root = inflater.inflate(R.layout.fragment_deposit, container, false);
-        mLeft = root.findViewById(R.id.deposit_lv_left);
-        mData = root.findViewById(R.id.deposit_lv_data);
-        mDataHorizontal = root.findViewById(R.id.deposit_data_horizontal);
-        mHeaderHorizontal = root.findViewById(R.id.deposit_header_horizontal);
-        mDataHorizontal.setScrollView(mHeaderHorizontal);
-        mHeaderHorizontal.setScrollView(mDataHorizontal);
 
-        initView();
+        tvLeftTitle = root.findViewById(R.id.tv_left_title);
+        rvTabRight = root.findViewById(R.id.rv_tab_right);
+        llTopRoot = root.findViewById(R.id.ll_top_root);
+        recyclerContent = root.findViewById(R.id.recycler_content);
+        swipeRefresh = root.findViewById(R.id.swipe_refresh);
+
+        //处理顶部标题部分
+        LinearLayoutManager linearLayoutManager = new LinearLayoutManager(requireContext());
+        linearLayoutManager.setOrientation(LinearLayoutManager.HORIZONTAL);
+        rvTabRight.setLayoutManager(linearLayoutManager);
+        TopTabAdpater topTabAdpater = new TopTabAdpater(requireContext());
+        rvTabRight.setAdapter(topTabAdpater);
+        for (int i = 0; i < 50; i++) {
+            topTabs.add("类型" + i);
+        }
+        topTabAdpater.setDatas(topTabs);
+        //处理内容部分
+        recyclerContent.setLayoutManager(new LinearLayoutManager(requireContext()));
+        recyclerContent.setHasFixedSize(true);
+        final ContentAdapter contentAdapter = new ContentAdapter(requireContext(),rvTabRight);
+        recyclerContent.setAdapter(contentAdapter);
+
+        recyclerContent.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                for (int i = 0; i < 30; i++) {
+                    Entity entity = new Entity();
+                    entity.setLeftTitle("贵州茅台" + i);
+                    rightMoveDatas.clear();
+                    for (int j = 0; j < 50; j++) {
+                        rightMoveDatas.add("年份" + j);
+                    }
+                    entity.setRightDatas(rightMoveDatas);
+                    mEntities.add(entity);
+                }
+                contentAdapter.setDatas(mEntities);
+                Toast.makeText(requireContext(), "加载完毕,加载了30条数据", Toast.LENGTH_SHORT).show();
+            }
+        }, 1500);
+
+        //下拉刷新
+        swipeRefresh.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
+            @Override
+            public void onRefresh() {
+                recyclerContent.postDelayed(new Runnable() {
+                    @Override
+                    public void run() {
+                        for (int i = 0; i < 50; i++) {
+                            Entity entity = new Entity();
+                            entity.setLeftTitle("贵州茅台" + i);
+                            rightMoveDatas.clear();
+                            for (int j = 0; j < 50; j++) {
+                                rightMoveDatas.add("年份" + j);
+                            }
+                            entity.setRightDatas(rightMoveDatas);
+                            mEntities.add(entity);
+                        }
+                        contentAdapter.setDatas(mEntities);
+                        swipeRefresh.setRefreshing(false);
+                        Toast.makeText(requireContext(), "刷新完毕,刷新了50条数据", Toast.LENGTH_SHORT).show();
+                    }
+                }, 1500);
+            }
+        });
 
         return root;
-    }
-
-    private void initView() {
-        mListData = depositViewModel.getSampleData();
-
-        mRecordDataLeftAdapter = new RecordDataLeftAdapter(mListData);
-        mLeft.setAdapter(mRecordDataLeftAdapter);
-
-        mRecordDataAdapter = new RecordDataAdapter(mListData);
-        mData.setAdapter(mRecordDataAdapter);
     }
 }
